06c0bbff4541f8670c629fc84dca5f3d910cb5a3
nichcream <c.potato23@gmail.com>
2018-01-26 21:46:50 +0800

    land: Remove EGL symlinks

--- land/Android.mk
+++ land/Android.mk
@@ -440,7 +440,4 @@ LOCAL_MODULE_SUFFIX := .jar
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-
 endif
